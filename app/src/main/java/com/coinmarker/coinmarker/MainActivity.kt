9d79814999eb923aa031c25b938b145a707c408f
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-15 17:39:38 +0900

    [chore] BindingActivity, BindingFragment 추가 및 적용

--- app/src/main/java/com/coinmarker/coinmarker/MainActivity.kt
+++ app/src/main/java/com/coinmarker/coinmarker/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.coinmarker.coinmarker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.artventure.artventure.binding.BindingActivity
+import com.coinmarker.coinmarker.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
