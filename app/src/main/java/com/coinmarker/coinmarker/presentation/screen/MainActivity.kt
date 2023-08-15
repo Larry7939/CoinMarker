7c235eb749ff3d2417fdd93a8008e8340c942193
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-15 18:37:07 +0900

    [mod] MainActivity 패키지 이동 coinmarker -> screen

--- app/src/main/java/com/coinmarker/coinmarker/presentation/screen/MainActivity.kt
+++ app/src/main/java/com/coinmarker/coinmarker/presentation/screen/MainActivity.kt
@@ -1,3 +1,11 @@
+package com.coinmarker.coinmarker.presentation.screen
+
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.artventure.artventure.binding.BindingActivity
+import com.coinmarker.coinmarker.R
+import com.coinmarker.coinmarker.databinding.ActivityMainBinding
+
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
