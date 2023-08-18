5d596c3f144a644a48ef7d09ce34d0968ca07611
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-18 10:06:44 +0900

    [feat] 퍼센테이지 색상 표기 구현

--- app/src/main/java/com/coinmarker/coinmarker/presentation/adapter/MarketAdapter.kt
+++ app/src/main/java/com/coinmarker/coinmarker/presentation/adapter/MarketAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.coinmarker.coinmarker.R
 import com.coinmarker.coinmarker.data.model.AssetDto
 import com.coinmarker.coinmarker.databinding.ItemAssetSearchResultBinding
 import com.coinmarker.coinmarker.presentation.util.diffutil.AssetItemCallback
@@ -33,6 +34,18 @@ class MarketAdapter(
                 binding.ibAssetIsArchived.setOnClickListener {
                     it.isSelected = !it.isSelected
                     updateArchiveState(data, it.isSelected)
+                }
+                if(data.changePrice.first() == '-'){
+                    binding.tvChangeRate.setTextColor(context.resources.getColor(R.color.blue,null))
+                    binding.tvChangePrice.setTextColor(context.resources.getColor(R.color.blue,null))
+                }
+                else if(data.changePrice.first() == '0'){
+                    binding.tvChangeRate.setTextColor(context.resources.getColor(R.color.B1,null))
+                    binding.tvChangePrice.setTextColor(context.resources.getColor(R.color.B1,null))
+                }
+                else{
+                    binding.tvChangeRate.setTextColor(context.resources.getColor(R.color.red,null))
+                    binding.tvChangePrice.setTextColor(context.resources.getColor(R.color.red,null))
                 }
             }
         }
