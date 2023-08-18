3d9e9b8ae7d4c3624261bd9976007f61b01a3a1c
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-18 21:47:22 +0900

    [del] 불필요한 의존성 삭제 및 리팩토링

--- app/src/main/java/com/coinmarker/coinmarker/data/model/response/ResponseGetAssetDetail.kt
+++ app/src/main/java/com/coinmarker/coinmarker/data/model/response/ResponseGetAssetDetail.kt
@@ -1,14 +1,14 @@
 package com.coinmarker.coinmarker.data.model.response
 
-data class ResponseGetAssetDetail (
-        val ask: String,
-        val bid: String,
-        val change: String,
-        val changePercent: String,
-        val high: String,
-        val last: String,
-        val low: String,
-        val `open`: String,
-        val timestamp: Long,
-        val volume: String
+data class ResponseGetAssetDetail(
+    val ask: String,
+    val bid: String,
+    val change: String,
+    val changePercent: String,
+    val high: String,
+    val last: String,
+    val low: String,
+    val `open`: String,
+    val timestamp: Long,
+    val volume: String
 )
