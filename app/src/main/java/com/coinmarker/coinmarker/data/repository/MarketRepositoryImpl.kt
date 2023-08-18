2af9d8ecd0b51c0fdcba08d62311a70a25eaf94f
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-18 19:42:23 +0900

    [fix] 가격 및 퍼센트 숫자 포맷 수정

--- app/src/main/java/com/coinmarker/coinmarker/data/repository/MarketRepositoryImpl.kt
+++ app/src/main/java/com/coinmarker/coinmarker/data/repository/MarketRepositoryImpl.kt
@@ -3,7 +3,8 @@ package com.coinmarker.coinmarker.data.repository
 import com.coinmarker.coinmarker.data.model.AssetDto
 import com.coinmarker.coinmarker.data.model.response.ResponseGetAssetDetail
 import com.coinmarker.coinmarker.data.source.MarketDataSource
-import com.coinmarker.coinmarker.data.util.addCommasToNumber
+import com.coinmarker.coinmarker.data.util.addCommasToPrice
+import com.coinmarker.coinmarker.data.util.addCommasToVolume
 import com.coinmarker.coinmarker.data.util.setPercentFormat
 import com.coinmarker.coinmarker.data.util.setPriceFormat
 import com.coinmarker.coinmarker.data.util.setVolumeFormat
@@ -29,11 +30,11 @@ class MarketRepositoryImpl @Inject constructor(private val marketDataSource: Mar
             with(entry) {
                 assets.add(
                     AssetDto(
-                        currencyPair = key.replace('_', '/').uppercase(Locale.ROOT),
-                        last = value.last.addCommasToNumber().setPriceFormat(),
-                        changePercent = value.changePercent.addCommasToNumber().setPercentFormat(),
-                        changePrice = value.change.addCommasToNumber().setPriceFormat(),
-                        volume = value.volume.setVolumeFormat().addCommasToNumber(),
+                        currencyPair = key.replace('_', '/').uppercase(Locale.ROOT), //finished
+                        last = value.last.setPriceFormat().addCommasToPrice(), //finished
+                        changePercent = value.changePercent.setPercentFormat(),
+                        changePrice = value.change.setPriceFormat().addCommasToPrice(),
+                        volume = value.volume.setVolumeFormat().addCommasToVolume(),
                         timeStamp = value.timestamp
                     )
                 )
--- app/src/main/java/com/coinmarker/coinmarker/data/util/StringExt.kt
+++ app/src/main/java/com/coinmarker/coinmarker/data/util/StringExt.kt
@@ -1,19 +1,32 @@
 package com.coinmarker.coinmarker.data.util
 
+import timber.log.Timber
+import java.lang.Math.abs
+import java.math.BigDecimal
 import java.text.DecimalFormat
 
-fun String.addCommasToNumber(): String {
+
+fun String.addCommasToPrice(): String {
+    val number = this.toIntOrNull()
+    if (number != null) {
+        val decimalFormat = DecimalFormat("#,###")
+        return decimalFormat.format(number)
+    }
+    return this
+}
+fun String.addCommasToVolume(): String {
     val number = this.toDoubleOrNull()
     if (number != null) {
-        val numberFormat = DecimalFormat("#,###")
-        return numberFormat.format(number)
+        val decimalFormat = DecimalFormat("#,###")
+        return decimalFormat.format(number)
     }
     return this
 }
 fun String.setPercentFormat(): String {
     val number = this.toDoubleOrNull()
     if (number != null) {
-        return String.format("%.2f", number)
+        val decimalFormat = DecimalFormat("#.##")
+        return decimalFormat.format(number)
     }
     return this
 }
@@ -21,10 +34,15 @@ fun String.setPercentFormat(): String {
 fun String.setPriceFormat(): String {
     val number = this.toDoubleOrNull()
     if (number != null) {
-        return if (number > 100) {
-            String.format("%.0f", number)
+        return if (abs(number) >= 100) {
+            number.toInt().toString()
         } else {
-            String.format("%.2f", number)
+            val essencePart = BigDecimal(number.toString()).toPlainString().substringBefore('.')
+            val decimalPart = BigDecimal(number.toString()).toPlainString().substringAfter('.')
+            Timber.d("===== essence $essencePart")
+            Timber.d("===== decimal $decimalPart")
+            "$essencePart.${decimalPart.getOrElse(0){'0'}}${decimalPart.getOrElse(1){'0'}}"
+
         }
     }
     return this
